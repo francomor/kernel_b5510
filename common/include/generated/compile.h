504e89284d5a834dff898da6828ee8e137f5ea66
francomor <francomorero96@gmail.com>
2014-03-10 17:05:15 -0300

    first commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 10 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 PREEMPT Wed Mar 5 20:44:11 ART 2014"
+#define LINUX_COMPILE_TIME "20:44:11"
+#define LINUX_COMPILE_BY "francomor"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
